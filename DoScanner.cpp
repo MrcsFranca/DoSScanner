8117c6e29a3900e1597ae8580fa59a426ad1ca74
MrcsFranca <francamarcos035@gmail.com>
2025-06-23 10:05:02 -0300

    Passando a usar filas

--- DoScanner.cpp
+++ DoScanner.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <chrono>
-#include <ctime>
-#include <stack>
-#include <vector>
 #include <string>
+#include <ctime>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-const int QUANT_REQUISICOES = 10, LIMITE_IP = 6, INTERVALO_SEGUNDOS = 5;
+const int QUEUE_LIMIT = 10, IP_LIMIT = 6, SECONDS = 5;
 
 struct requestData {
     string ip;
@@ -16,41 +15,47 @@ struct requestData {
 
 class Detector {
     private:
-            stack<requestData> pilhaRequisicoes;
+            queue<requestData> filaRequisicoes;
     public:
         void saveRequest(requestData req) {
             time_t now = time(NULL);
             req.timeStamp = now;
-            pilhaRequisicoes.push(req);
+            filaRequisicoes.push(req);
 
-            if(pilhaRequisicoes.size() > QUANT_REQUISICOES) {
-                vector<requestData> temp;
-                while(pilhaRequisicoes.size() > 1) {
-                    temp.push_back(pilhaRequisicoes.top());
-                    pilhaRequisicoes.pop();
-                }
-                pilhaRequisicoes.pop();
-                for(auto it = temp.begin(); it != temp.end(); it++) {
-                    pilhaRequisicoes.push(*it);
+            if(filaRequisicoes.size() > QUEUE_LIMIT) {
+                filaRequisicoes.pop();
+            }
+            
+            cout << "Requisição registrada de " << req.ip << " em " << ctime(&req.timeStamp) << endl;
+            verifyDos(req);
+        }
+
+        void verifyDos(requestData req) {
+            if(filaRequisicoes.size() < IP_LIMIT) {
+                return;
+            }
+
+            vector<requestData> aux;
+            while(!filaRequisicoes.empty()) {
+                aux.push_back(filaRequisicoes.front());
+                filaRequisicoes.pop();
+            }
+            
+            int numberOfResquests = 1;
+            bool detectedAttack = false;
+            requestData mostRecentRequest = aux.front();
+            for(int i = 1; (i <= IP_LIMIT) && (detectedAttack == false); i++) {
+                if((difftime((time_t) (mostRecentRequest.timeStamp), aux[i].timeStamp) <= SECONDS) && (mostRecentRequest.ip == aux[i].ip)) {
+                    cout << "numero : " << numberOfResquests << endl;
+                    numberOfResquests++;
+                    if(numberOfResquests >= IP_LIMIT) {
+                        detectedAttack = true;
+                    }
                 }
             }
-            stack<requestData> temp;
-            requestData aux;
-        }
 
-        /*
-        void verifyDos() {
-
-        }
-        */
-        
-        
-        void imprimePilha() {
-            requestData aux;
-            while(pilhaRequisicoes.size() > 0) {
-                aux = pilhaRequisicoes.top();
-                cout << aux.ip << " : " << ctime(&aux.timeStamp) << endl;
-                pilhaRequisicoes.pop();
+            if(detectedAttack) {
+                cout << "Possivel ataque de " << req.ip << endl << IP_LIMIT << " requisicoes em menos de " << SECONDS << " segundos" << endl;
             }
         }
 };
@@ -63,6 +68,4 @@ int main() {
         req.ip = "192.168.0." + to_string(i);
         scanner.saveRequest(req);
     }
-    scanner.imprimePilha();
-    
 }
