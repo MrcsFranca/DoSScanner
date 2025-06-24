1ae1b30abe128c775731b462e529f7ee13d07095
MrcsFranca <francamarcos035@gmail.com>
2025-06-24 11:27:05 -0300

    adicionando leitura e salvamento de arquivos

--- DoScanner.cpp
+++ DoScanner.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <queue>
 #include <fstream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -15,28 +16,59 @@ struct requestData {
 };
 
 class FileManager {
+    private:
+        unordered_map<string, bool> blocked;
     public:
         void logRequest(requestData req) {
             ofstream log;
             try {
-                log.open("requests.txt", ios::app);
+                log.open("requests.log", ios::app);
                 if(!log) {
                     throw new runtime_error("erro");
                 } else {
                     log << req.ip << " -- " << ctime(&req.timeStamp) << endl;
                 }
             } catch(runtime_error) {
-                cout << "erro ao abrir arquivo de ips bloqueados" << endl;
+                cout << "erro ao abrir arquivo de logging" << endl;
             }
             log.close();
+        }
+
+        void saveBlockedIps(requestData req) {
+            ofstream file;
+            try {
+                file.open("blocked_ips.txt", ios::app);
+                if(!file) {
+                    throw new runtime_error("erro");
+                } else {
+                    file << req.ip << endl;
+                }
+            } catch(runtime_error) {
+                cout << "erro ao abrir o arquivo de ips bloqueados" << endl;
+            }
+            file.close();
+        }
+
+        void loadBlockedIps() {
+            ifstream file("blocked_ips.txt");
+            string blockedIp;
+            while(getline(file, blockedIp)) {
+                blocked[blockedIp] = true;
+            }
+            file.close();
+        }
+
+        unordered_map<string, bool> getBlockedList() {
+            return blocked;
         }
 };
 
 class Detector {
     private:
             queue<requestData> requestQueue;
+            FileManager manager;
     public:
-        void saveRequest(requestData req) {
+        void saveRequest(requestData req, unordered_map<string, bool> blocked) {
             time_t now = time(NULL);
             req.timeStamp = now;
             requestQueue.push(req);
@@ -46,8 +78,13 @@ class Detector {
             }
             
             cout << "Requisição registrada de " << req.ip << " em " << ctime(&req.timeStamp) << endl;
-            FileManager manager;
             manager.logRequest(req);
+
+            if(blocked[req.ip]) {
+                cout << "@@@" << req.ip << " está fazendo requisições, porém está bloqueado" << endl;
+                return;
+            }
+
             verifyDos(req);
         }
 
@@ -67,7 +104,6 @@ class Detector {
             requestData mostRecentRequest = aux.front();
             for(int i = 1; (i <= IP_LIMIT) && (detectedAttack == false); i++) {
                 if((difftime((time_t) (mostRecentRequest.timeStamp), aux[i].timeStamp) <= SECONDS) && (mostRecentRequest.ip == aux[i].ip)) {
-                    cout << "numero : " << numberOfResquests << endl;
                     numberOfResquests++;
                     if(numberOfResquests >= IP_LIMIT) {
                         detectedAttack = true;
@@ -77,6 +113,8 @@ class Detector {
 
             if(detectedAttack) {
                 cout << "Possivel ataque de " << req.ip << endl << IP_LIMIT << " requisicoes em menos de " << SECONDS << " segundos" << endl;
+                manager.saveBlockedIps(req);
+                //system("sleep 10");
             }
         }
 };
@@ -85,10 +123,15 @@ class Detector {
 
 int main() {
     Detector scanner;
+    FileManager manager;
     requestData req;
+
+    srand(time(NULL));
+
     req.timeStamp = 0;
-    for(int i = 0; i < 13; i++) {
-        req.ip = "192.168.0." + to_string(i);
-        scanner.saveRequest(req);
+    while(true) {
+        req.ip = "192.168.0." + to_string((rand() % 256));
+        manager.loadBlockedIps();
+        scanner.saveRequest(req, manager.getBlockedList());
     }
 }
