ba5b56463de57e766686e5e3c6224a068563e491
MrcsFranca <francamarcos035@gmail.com>
2025-07-02 14:28:23 -0300

    Adicionando captura de pacotes

--- DoScanner.cpp
+++ DoScanner.cpp
@@ -5,6 +5,10 @@
 #include <queue>
 #include <fstream>
 #include <unordered_map>
+#include <pcapplusplus/PcapLiveDeviceList.h>
+#include <pcapplusplus/SystemUtils.h>
+#include <pcapplusplus/Packet.h>
+#include <pcapplusplus/IPv4Layer.h>
 
 using namespace std;
 
@@ -21,30 +25,22 @@ class FileManager {
     public:
         void logRequest(requestData req) {
             ofstream log;
-            try {
-                log.open("requests.log", ios::app);
-                if(!log) {
-                    throw new runtime_error("erro");
-                } else {
-                    log << req.ip << " -- " << ctime(&req.timeStamp) << endl;
-                }
-            } catch(runtime_error) {
-                cout << "erro ao abrir arquivo de logging" << endl;
+            log.open("requests.log", ios::app);
+            if(!log) {
+                throw new runtime_error("erro");
+            } else {
+                log << req.ip << " -- " << ctime(&req.timeStamp) << endl;
             }
             log.close();
         }
 
         void saveBlockedIps(requestData req) {
             ofstream file;
-            try {
-                file.open("blocked_ips.txt", ios::app);
-                if(!file) {
-                    throw new runtime_error("erro");
-                } else {
-                    file << req.ip << endl;
-                }
-            } catch(runtime_error) {
-                cout << "erro ao abrir o arquivo de ips bloqueados" << endl;
+            file.open("blocked_ips.txt", ios::app);
+            if(!file) {
+                throw new runtime_error("erro");
+            } else {
+                file << req.ip << endl;
             }
             file.close();
         }
@@ -65,27 +61,34 @@ class FileManager {
 
 class Detector {
     private:
-            queue<requestData> requestQueue;
-            FileManager manager;
+        FileManager manager;
+        requestData req;
+        queue<requestData> requestQueue;
+        vector<pcpp::PcapLiveDevice*> devList = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDevicesList();
+        pcpp::PcapLiveDevice *device = devList[0];
     public:
         void saveRequest(requestData req, unordered_map<string, bool> blocked) {
             time_t now = time(NULL);
             req.timeStamp = now;
             requestQueue.push(req);
 
-            if(requestQueue.size() > QUEUE_LIMIT) {
+            if(requestQueue.size() >= QUEUE_LIMIT) {
                 requestQueue.pop();
             }
-            
-            cout << "Requisição registrada de " << req.ip << " em " << ctime(&req.timeStamp) << endl;
-            manager.logRequest(req);
 
             if(blocked[req.ip]) {
-                cout << "@@@" << req.ip << " está fazendo requisições, porém está bloqueado" << endl;
+                cout << "\033[33m[AVISO]   " << req.ip << " está fazendo requisições, porém está bloqueado" << endl << endl;
                 return;
+            } else {
+                clog << "\033[32m[INFO]    Requisição registrada de " << req.ip << " em " << ctime(&req.timeStamp) << endl;
+                try {
+                    manager.logRequest(req);
+                } catch(const exception&) {{
+                    cerr << "[ERRO]\tErro ao abrir arquivo de log" << endl;
+                }}
+                verifyDos(req);
             }
 
-            verifyDos(req);
         }
 
         void verifyDos(requestData req) {
@@ -112,26 +115,58 @@ class Detector {
             }
 
             if(detectedAttack) {
-                cout << "Possivel ataque de " << req.ip << endl << IP_LIMIT << " requisicoes em menos de " << SECONDS << " segundos" << endl;
-                manager.saveBlockedIps(req);
-                //system("sleep 10");
+                cout << "\033[31m[ALERTA]  Possivel ataque de " << req.ip << endl << endl;
+                try {
+                    manager.saveBlockedIps(req);
+                } catch(const exception&) {
+                    cerr << "[ERRO]\tErro ao salvar ip bloqueado" << endl;
+                }
+            }
+        }
+
+        void startCapturing() {
+            if(devList.empty()) {
+                cerr << "[ERRO]\tNenhuma interface de rede encontrada" << endl;
+            }
+            if(device == NULL) {
+                cerr << "[ERRO]\tErro ao encontrar interface de captura de pacotes" << endl;
+            }
+            if(!device->open()) {
+                cerr << "[ERRO]\tErro ao abrir interface de captura de pacores" << endl;
+            }
+            cout << "Capturando pacotes com a interface: " << device->getName() << endl;
+
+            device->startCapture(onPacketArrives, this);
+
+            while(true) {
+                pcpp::multiPlatformSleep(1);
+            }
+            device->stopCapture();
+        }
+
+        static void onPacketArrives(pcpp::RawPacket *rawPacket, pcpp::PcapLiveDevice *device, void *cookie) {
+            auto *dec = static_cast<Detector*>(cookie);
+            try {
+                dec->manager.loadBlockedIps();
+            } catch(const exception&) {
+                cerr << "Erro ao abrir arquivos de IPs bloqueados" << endl;
+            }
+            pcpp::Packet packet(rawPacket);
+
+            if(packet.isPacketOfType(pcpp::IPv4)) {
+                pcpp::IPv4Layer *ip = packet.getLayerOfType<pcpp::IPv4Layer>();
+                if(ip) {
+                    dec->req.ip = ip->getSrcIPAddress().toString();
+                    dec->req.timeStamp = time(NULL);
+
+                    dec->saveRequest(dec->req, dec->manager.getBlockedList());
+                }
             }
         }
 };
 
-
-
 int main() {
     Detector scanner;
-    FileManager manager;
-    requestData req;
-
-    srand(time(NULL));
-
-    req.timeStamp = 0;
-    while(true) {
-        req.ip = "192.168.0." + to_string((rand() % 256));
-        manager.loadBlockedIps();
-        scanner.saveRequest(req, manager.getBlockedList());
-    }
+    scanner.startCapturing();
+    return 0;
 }
